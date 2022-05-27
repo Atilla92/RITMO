d164e93c20bb9031a033f4445f42fe42163f0c98
Atilla Vrasdonk <atillajv@eduroam-193-157-238-144.wlan.uio.no>
2022-05-27 13:48:22 +0200

    renaming files, starting analysis audio

---
+++ PILOT_SEV_APRIL_2022/analysisAudio.py
@@ -0,0 +1,20 @@
+import glob
+from scipy.io.wavfile import read
+
+name_files = []
+audio_files = []
+
+for filepath in glob.iglob('Audio/*.wav'):
+    if filepath.endswith('.wav'):
+        # filepath_split = filepath.partition('/')
+        # filepath_split = filepath_split[2].partition('/')
+        # filepath_split = filepath_split[2].partition('-')
+        audio_files.append(filepath)
+
+
+print (audio_files[0])
+
+
+#for i, item in enumerate(audio_files):
+
+#data_audio = read(str(audio_files[0]))
