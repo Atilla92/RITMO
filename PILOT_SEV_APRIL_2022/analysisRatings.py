44002006825cf394edcf02333e4e676f56463d79
Atilla Vrasdonk <atillajv@eduroam-193-157-172-132.wlan.uio.no>
2022-06-07 17:30:23 +0200

    entropy analysis of audio

--- PILOT_SEV_APRIL_2022/analysisRatings.py
+++ PILOT_SEV_APRIL_2022/analysisRatings.py
@@ -5,23 +5,24 @@ import os
 import scipy as sp
 import numpy as np
 from scipy.io.wavfile import read
+from scipy.stats import entropy
+from scipy.stats import norm
 
-### Initiate variables
-name_files = []
-audio_files = []
-FSR_files = []
-step_number = 0
-empty_dict = []
+
+
+# Initial Variables
+
+time_shift = -2
 
 ### Fetch all files for analysis
 path = '/Users/atillajv/CODE/FILES/PILOT_SEV_APRIL_2022/Audio'
 paths_audio = glob.glob(os.path.join(path, '*.wav'))
 
 audio_files = os.listdir(path)
-print(paths_audio)
+#print(paths_audio)
 # print(filelist)
-fig = plt.figure()
-filelist = []
+
+#filelist = []
 for filepath in glob.iglob('Ratings/'+'*.csv'):
     
     
@@ -29,21 +30,70 @@ for filepath in glob.iglob('Ratings/'+'*.csv'):
     # filelist.append(filepath)
 
    # if (filepath.endswith('_IMPRO.csv') and  'R1' in file_split and 'P3' in file_split):
-    if (filepath.endswith('_IMPRO.csv')):
-        current_file  = filepath.rsplit('_', 1)[0].rsplit('/')[1].split('_',1)[1] 
-        df= pd.read_csv(filepath, sep = ',')
-       
-        path_audio_file = path + '/' + current_file + '.wav'
-        audio_data = read(path_audio_file)[1][100]
-        print(audio_data)
-        x_array = np.arange(len(audio_data))
     
-       # print (filepath) 
-        #interpolator = sp.interpolate.interp1d(df['Time'], df[' Value'], kind = 'zero') 
-        #plt.scatter(df['Time'], df[' Value'], color = 'red')
-        #plt.step(df['Time'], df[' Value'], where='post')
-        plt.plot(x_array,audio_data)
+    if (filepath.endswith('_IMPRO.csv')):
+        #fig = plt.figure()
+        f, axs = plt.subplots(3, 1, sharex=True, sharey=False)
+        df= pd.read_csv(filepath, sep = ',')
+        df['Time'] = df['Time'] + time_shift
+        print(df)
+        current_file  = filepath.rsplit('_', 1)[0].rsplit('/')[1].split('_',1)[1] 
+        path_audio_file = path + '/' + current_file + '.wav'
+        sampleRate =  read(path_audio_file)[0]
+        audio_data = read(path_audio_file)[1]
+        lst1_audio = [item[0] for item in audio_data]
+        #print(audio_data)
+        x_array = np.arange(len(audio_data))
+        x_array = np.divide(x_array,sampleRate)
+
+    
+        axs[0].plot(x_array,np.divide(lst1_audio,1))
+        axs[1].step(df['Time'] , df[' Value'], where='post')
+
+
+        t_start = 0
+        entropy_values = []
+        for i in df['Time']:
+            axs[0].axvline(x=(i), color ='red')
+            # Computing entropy
+            #t_end = i
+            
+            t_start_index = np.argmax(x_array>= t_start)
+            t_end_index = np.argmax (x_array >= i)
+            print (t_start, 'start', i, 'end')
+
+            audio_segment = lst1_audio[t_start_index:t_end_index]
+            x_array_segment = x_array[t_start_index:t_end_index]
+
+            counts, bins = np.histogram(audio_segment, bins = 10, density= True)
+            mu, sigma = norm.fit(audio_segment)
+            pdf_hist  = counts/ sum(counts)
+            entropy_hist = entropy(pdf_hist, base = 2)
+            print(entropy_hist,'ENTROPY Value')
+            entropy_values.append(entropy_hist)
+
+            t_start = i
+        
+        axs[2].step(df['Time'] , entropy_values, where='pre')
+
+        # need to normalize values and plot on the same graph, and store in df, see whether makes sense.
+        # Make one big df, where everything is stored. Will get many data points.  
+        plt.show()
+
+            
+    
+
+            
+        
+
+        
+        # Compute entropy
+
+
         #plt.show()
+
+
+
     # if filepath.endswith('_FLOW.csv'):
     #     plt.scatter(df['Time'], df[' Value'], color = 'blue')
 
