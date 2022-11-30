c5f890f13a23fa204879a1f6d8cf78eedf7c84c7
Atilla Vrasdonk <atillajv@Atillas-MacBook-Pro-2.local>
2022-11-30 12:36:24 +0100

    Merge all files

--- ENTROPY/ELANanalysis.py
+++ ENTROPY/ELANanalysis.py
@@ -7,7 +7,7 @@ import glob
 #And have to add the response, from csv for subjective ratings. Does that make sense?
 
 # One file or all in a specific folder
-file_name = 'P4_D5_G2_M5_R2_T1' # Name file if loop_on = False
+file_name = 'P6_D1_G1_M6_R1_T1' # Name file if loop_on = False
 loop_on = True # True if you want to loop through folder
 path_files = '/Users/atillajv/CODE/RITMO/FILES/ELAN/'
 file_output = '/Users/atillajv/CODE/RITMO/ENTROPY/output/main/28_Nov_2022/'
@@ -32,7 +32,32 @@ print(list_files)
 
 # Initiate dataframe before loop 
 store_i = 0
-df_store = pd.DataFrame(columns=['Name', 'Music_Imp', 'Dance_Imp', 'Baile_Level','Guitarra_Level' ,'Step', 'Imp_Av', 'CTW_Av', 'LZ_Av','Assigned_Cat', 'Rounds'])
+df_store = pd.DataFrame(columns=['Name', 'Music_Imp', 'Dance_Imp', 'Baile_Level','Guitarra_Level' ,'Step', 'Imp_Av', 'CTW', 'LZ','Assigned_Cat', 'Rounds', 
+'Q1a',
+'Q1b',
+'Q2a',
+'Q2b',
+'Q2c',
+'Q2d',
+'Q2e',
+'Q2f',
+'Q2g',
+'Q2h',
+'Q2i',
+'Q2j',
+'Q3a',
+'Q3b',
+'Q4a',
+'Q4b',
+'Abs_Av',
+'Perf_Av',
+'SFS',
+'Dance_mode',
+'Music_mode',
+'Palo',
+'LZ_Av',
+'CTW_Av'
+])
 
 for file_i, file_item in enumerate(list_files):
     print('FILE BEING ANALYSED: ', file_item)
@@ -41,15 +66,9 @@ for file_i, file_item in enumerate(list_files):
     dfR = pd.read_csv( '/Users/atillajv/CODE/RITMO/FILES/Ratings/'+ prefix +'_'+ file_item +'_IMPRO.csv' )
     #dfE = pd.read_csv('/Users/atillajv/CODE/RITMO/ENTROPY/output/main/Entropy_LZ_CTW_(w=4000_s=[]_ds=4_b=on_abs=on_t0=0)_'+file_item+'.csv')[1:]
     dfE = pd.read_csv('/Users/atillajv/CODE/RITMO/ENTROPY/output/main/28_Nov_2022/'+file_item+'.csv')[1:]
-    
+    dfS = pd.read_csv('/Users/atillajv/CODE/RITMO/FILES/Subjective/DuringExperiments_Sevilla_06102022_DropW_Entropy.csv')
     #dfF =  pd.read_csv( '/Users/atillajv/CODE/RITMO/FILES/Ratings/P3_'+ file_name +'_FLOW.csv' )
     #print(dfE)
-
-
-    # Add first row with time 0
-    dfR.loc[-1] =[0, dfR[' Value'].iloc[0]]
-    dfR.index = dfR.index + 1  # shifting index
-    dfR = dfR.sort_index()  # sorting by index
 
 
     # Estimate interval 
@@ -64,13 +83,19 @@ for file_i, file_item in enumerate(list_files):
     #Append end time to last row 
     dfR.loc[len(dfR.index)] = [tI_1.iloc[-1] ,dfR[' Value'].iloc[-1]]
 
+
     #Estimate which values fall within interval. 
 
     # Move ratings to the left 1/2 round
  
     #dt_left = dtI/(rounds * frac_round)
-    tR_end = dfR['Time'] - dt_L
+    tR_end = dfR['Time'] - dt_L    
+    # Add first row with time 0
+    tR_end.loc[-1] = 0
+    tR_end.index = tR_end.index + 1  # shifting index
+    tR_end = tR_end.sort_index()  # sorting by index
     tR_end.loc[len(tR_end.index)] = tI_1.iloc[-1]
+    print(tR_end)
 
 
     # Start a loop
@@ -85,10 +110,12 @@ for file_i, file_item in enumerate(list_files):
     for i in np.arange(len(dfI)):
 
         for j in np.arange(n,len(dfR)):
+
             if tR_end.iloc[j] == 0 and tR_end.iloc[j+1] < tI_0.iloc[i]:
                 continue
 
             elif tR_end.iloc[j]< tI_0.iloc[i] and tR_end.iloc[j+1] >tI_0.iloc[i]:
+                
                 mean_array.append(dfR[' Value'].iloc[j])
                 if tR_end.iloc[j+1] < tI_1.iloc[i]:
                     proportion_array.append(np.divide(tR_end.iloc[j+1]-tI_0.iloc[i] ,dtI_2.iloc[i]))
@@ -96,20 +123,23 @@ for file_i, file_item in enumerate(list_files):
                     proportion_array.append(1)
             
             elif tR_end.iloc[j] > tI_0.iloc[i] and tR_end.iloc[j+1] < tI_1.iloc[i]:
+                
                 mean_array.append(dfR[' Value'].iloc[j])
                 proportion_array.append(np.divide(tR_end.iloc[j+1]-tR_end.iloc[j] ,dtI_2.iloc[i]))
 
             elif tR_end.iloc[j] < tI_1.iloc[i] and tR_end.iloc[j+1] >= tI_1.iloc[i]:
+                
                 mean_array.append(dfR[' Value'].iloc[j])
                 proportion_array.append(np.divide(tI_1.iloc[i]-tR_end.iloc[j] ,dtI_2.iloc[i]))
 
             elif tR_end.iloc[j] < tI_0.iloc[i] and tR_end.iloc[j+1] > tI_1.iloc[i]:
+                
                 mean_array.append(dfR[' Value'].iloc[j])
                 proportion_array.append(1)
             
 
             elif tR_end.iloc[j]>= tI_1.iloc[i]:
-                n=j-2
+                n=j-1
                 
                 break
         Imp_average.append(np.sum(np.multiply(mean_array,proportion_array)))
@@ -119,31 +149,56 @@ for file_i, file_item in enumerate(list_files):
         list_Entropy = dfE.loc[(dfE["t0"] >= tI_0.iloc[i] ) & (dfE['t0'] <= tI_1.iloc[i]  ) ]
         LZ_average.append(list_Entropy['LZ'].mean()) 
         CTW_average.append(list_Entropy['CTW'].mean())
+        dfTest = dfS[ (dfS['Name'].str.contains(file_item)) & (dfS['Participant'].str.contains(prefix)) ]
+        print(dfTest)
         df_store.loc[store_i] = {
             'Name': file_item,
             'Music_Imp': dfI['Music_Mode'].iloc[i],
             'Dance_Imp': dfI['Category'].iloc[i],
             'Step': dfI['Step'].iloc[i],
             'Imp_Av': np.sum(np.multiply(mean_array,proportion_array)),
-            'CTW_Av': list_Entropy['CTW'].mean() ,
-            'LZ_Av': list_Entropy['LZ'].mean(),
+            'CTW': list_Entropy['CTW'].mean() ,
+            'LZ': list_Entropy['LZ'].mean(),
             'Assigned_Cat': 'IMP0',
             'Baile_Level': dfI['Baile'].iloc[i],
             'Guitarra_Level': dfI['Guitarra'].iloc[i],
-            'Rounds':dfI['Bloques'].iloc[i]}
+            'Rounds':dfI['Bloques'].iloc[i],
+            "Q1a": dfTest["Q1a"].iloc[0],
+            "Q1b": dfTest["Q1b"].iloc[0],
+            "Q2a": dfTest["Q2a"].iloc[0],
+            "Q2b": dfTest["Q2b"].iloc[0],
+            "Q2c": dfTest["Q2c"].iloc[0],
+            "Q2d": dfTest["Q2d"].iloc[0],
+            "Q2e": dfTest["Q2e"].iloc[0],
+            "Q2f": dfTest["Q2f"].iloc[0],
+            "Q2g": dfTest["Q2g"].iloc[0],
+            "Q2h": dfTest["Q2h"].iloc[0],
+            "Q2i": dfTest["Q2i"].iloc[0],
+            "Q2j": dfTest["Q2j"].iloc[0],
+            "Q3a": dfTest["Q3a"].iloc[0],
+            "Q3b": dfTest["Q3b"].iloc[0],
+            "Q4a": dfTest["Q4a"].iloc[0],
+            "Q4b": dfTest["Q4b"].iloc[0],
+            "Abs_Av": dfTest["Abs_Av"].iloc[0],
+            "Perf_Av": dfTest["Perf_Av"].iloc[0],
+            "SFS": dfTest["SFS"].iloc[0],
+            "Dance_mode": dfTest["Dance_mode"].iloc[0],
+            "Music_mode": dfTest["Music_mode"].iloc[0],
+            "Palo": dfTest["Palo"].iloc[0],
+            "LZ_Av": dfTest["LZ"].iloc[0],
+            "CTW_Av": dfTest["CTW"].values
+            }
         store_i = store_i+1
         
+
 
 
         mean_array = []
         proportion_array = []
         
-
-
-    print(len(Imp_average), 'LZ:', LZ_average )
-
     dfI['Imp_Av'] = Imp_average
-    print(dfI)
+    dfTest = dfS[ (dfS['Name'].str.contains(file_name)) & (dfS['Participant'].str.contains(prefix)) ]
+    print(dfTest, 'TESTING!!!!!!!!')
 
     # Assign Category per IMPRO
     # IMP0 [0,3) Not improvised
@@ -178,5 +233,9 @@ def InfotoColumns(df):
 
 InfotoColumns(df_store)
 print(df_store)
-df_store.to_csv(file_output + 'Entropy_095' + '.csv')
+
+print(dfS)
+
+
+df_store.to_csv(file_output + 'Entropy_095_Subjective' + '.csv')
 
