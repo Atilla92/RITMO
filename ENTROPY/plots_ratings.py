16506d107911c7848a0778e42a6172596a3b9678
Atilla Vrasdonk <atillajv@Atillas-MacBook-Pro-2.local>
2023-04-26 17:46:51 +0200

    Ratings Plot Lausanne

--- ENTROPY/plots_ratings.py
+++ ENTROPY/plots_ratings.py
@@ -24,14 +24,16 @@ Indicarw:
 y_var = ' Value'
 file_input = '/Users/atillajv/CODE/RITMO/FILES/Ratings/'
 save_plot = '/Users/atillajv/CODE/RITMO/ENTROPY/output/plots/all_experiments_095/'
+audio_input = '/Users/atillajv/CODE/RITMO/ENTROPY/output/main/all_experiments_095_drums/'
+
 
 hue_var = 'Dance_mode'
-rating = 'FLOW'
-file_name = str(rating + "_t%_" + hue_var + '_filtered_dancer' )
+rating = 'IMPRO'
+file_name = str(rating + "_t%_" + hue_var + '_test' )
 
 # Loop or single file 
 loop_on = True #Set to false if only analysing one file. 
-loop_off = 'P7_D5_G1_M6_R1_T1.csv'
+loop_off = 'P3_P3_D5_G1_M6_R1_T1_FLOW.csv'
 filter_out = True
 
 # Initiate empty lists
@@ -54,34 +56,60 @@ print(entropy_files)
 
 plt.figure()
 j= 0
-for i, item in enumerate(entropy_files):
+for k, item_k in enumerate(entropy_files):
     name_list = []
+    print(item_k)
+
+    try:
+        dfR = pd.read_csv(file_input + item_k)
+        df_a = pd.read_csv(audio_input + item_k.split('_',1)[1].rpartition('_')[0] + '.csv')
+        #Drop first row with mean info
+        #dfR.drop(index=dfR.index[0], axis=0, inplace=True)
+
+        t_end =df_a['t0'].iloc[-1]
+        dfR['t_%'] = dfR['Time']/t_end
+        tR_end = dfR['t_%']
+        mean_array = []
+        time_array = []
+        tI_1 = np.arange(0.1, 1.2, 0.1)
+        time_r = []
+        n= 0
     
-    #print(i)
-    df_i = pd.read_csv(file_input + item)
-    #Drop first row with mean info
-    #df_i.drop(index=df_i.index[0], axis=0, inplace=True)
+        for i, item in enumerate(np.arange(0, 1.1, 0.1)):
+            for j in np.arange(n, len(dfR['t_%'])):
+                if tR_end.iloc[j]< tI_1[i] and tI_1[i]<1.1:
+                    mean_array.append(dfR[' Value'].iloc[j])
+                    time_r.append(dfR['Time'].iloc[j])
+                    time_array.append(tI_1[i])
 
-    t_end =df_i['Time'].iloc[-1]
-    df_i['t_%'] = df_i['Time']/t_end
-    #print(df_i)
+                elif tR_end.iloc[j] > 1:
+                    mean_array.append(dfR[' Value'].iloc[j])
+                    time_r.append(dfR['Time'].iloc[j])
+                    time_array.append(1.0)
+                    n=j
+                    break
+                
+                elif tR_end.iloc[j]> tI_1[i]:
+                    n = j
+                    break
+            
 
-    #plt.plot(df_i['t_%'], df_i['LZ'] )
-    array_x = np.array(df_i['t_%'])
-    array_y = np.array(df_i[y_var])
-    name_list.extend(repeat(str(item),len(np.array(df_i['Time']))))
-    #print(len(array_y))
-    #print(name_list)
-    df_plot = pd.DataFrame( {
-        'Name': name_list,
-        't_%': array_x,
-        'y_var': array_y,
-        't_0': np.array(df_i['Time'])
 
-    })
-    #print(df_plot)
-    df_plots = pd.concat([df_plots, df_plot], axis =0 )
+        name_list.extend(repeat(str(item_k),len(np.array(mean_array))))
+        #print(len(array_y))
+        #print(name_list)
+        df_plot = pd.DataFrame( {
+            'Name': name_list,
+            't_%': time_array,
+            'y_var': mean_array,
+            't_0': time_r
 
+        })
+        #print(df_plot)
+        df_plots = pd.concat([df_plots, df_plot], axis =0 )
+    except:
+        print('file not found:', item_k)
+        pass
 
 def InfotoColumns(df):
     dance_array = []
@@ -112,35 +140,35 @@ def InfotoColumns(df):
 
 
 InfotoColumns(df_plots)
-
 print(df_plots)
+
 
 # Binning
 df_plots['Assigned_%'] = 0
-df_plots.loc[df_plots['t_%']<= 0.10 , 'Assigned_%'] = 0.05
-df_plots.loc[(df_plots['t_%']<= 0.20) & (df_plots['t_%']> 0.10 ) , 'Assigned_%'] = 0.10
-df_plots.loc[(df_plots['t_%']<= 0.30) & (df_plots['t_%']> 0.20 ) , 'Assigned_%'] = 0.20
-df_plots.loc[(df_plots['t_%']<= 0.40) & (df_plots['t_%']> 0.30 ) , 'Assigned_%'] = 0.30
-df_plots.loc[(df_plots['t_%']<= 0.50) & (df_plots['t_%']> 0.40 ) , 'Assigned_%'] = 0.40
-df_plots.loc[(df_plots['t_%']<= 0.60) & (df_plots['t_%']> 0.50 ) , 'Assigned_%'] = 0.50
-df_plots.loc[(df_plots['t_%']<= 0.70) & (df_plots['t_%']> 0.60 ) , 'Assigned_%'] = 0.60
-df_plots.loc[(df_plots['t_%']<= 0.80) & (df_plots['t_%']> 0.70 ) , 'Assigned_%'] = 0.70
-df_plots.loc[(df_plots['t_%']<= 0.90) & (df_plots['t_%']> 0.80 ) , 'Assigned_%'] = 0.80
-df_plots.loc[df_plots['t_%']> 0.90  , 'Assigned_%'] = 0.90
+df_plots.loc[df_plots['t_%']<= 0.10 , 'Assigned_%'] = 0.10
+df_plots.loc[(df_plots['t_%']<= 0.20) & (df_plots['t_%']> 0.10 ) , 'Assigned_%'] = 0.20
+df_plots.loc[(df_plots['t_%']<= 0.30) & (df_plots['t_%']> 0.20 ) , 'Assigned_%'] = 0.30
+df_plots.loc[(df_plots['t_%']<= 0.40) & (df_plots['t_%']> 0.30 ) , 'Assigned_%'] = 0.40
+df_plots.loc[(df_plots['t_%']<= 0.50) & (df_plots['t_%']> 0.40 ) , 'Assigned_%'] = 0.50
+df_plots.loc[(df_plots['t_%']<= 0.60) & (df_plots['t_%']> 0.50 ) , 'Assigned_%'] = 0.60
+df_plots.loc[(df_plots['t_%']<= 0.70) & (df_plots['t_%']> 0.60 ) , 'Assigned_%'] = 0.70
+df_plots.loc[(df_plots['t_%']<= 0.80) & (df_plots['t_%']> 0.70 ) , 'Assigned_%'] = 0.80
+df_plots.loc[(df_plots['t_%']<= 0.90) & (df_plots['t_%']> 0.80 ) , 'Assigned_%'] = 0.90
+df_plots.loc[df_plots['t_%']> 0.90  , 'Assigned_%'] = 1.00
 
 #df_plots.to_csv('/Users/atillajv/CODE/RITMO/ENTROPY/output/plots/all_experiments_095/Test.csv')
 
 df_plots.drop_duplicates(subset=None, keep="first", inplace=True, ignore_index=True)
 if filter_out:
     df_plots = df_plots[~df_plots['Dance_mode'].str.contains("D0")]
-    df_plots = df_plots[~df_plots['Artist'].str.contains("G")]
+    #df_plots = df_plots[~df_plots['Artist'].str.contains("G")]
 #df_plots = df_plots[~df_plots['Dance_mode'].str.contains("D5")]
 print(df_plots)
 # Figures
 
 #sns.violinplot(data=df_plots, x="Assigned_%", y="LZ", hue = 'Dance_mode')
 fig = sns.lineplot(data = df_plots,  x="Assigned_%", y="y_var", hue = hue_var)
-fig.set(xlabel='t [%]', ylabel = 'IMPRO', title = rating)
+fig.set(xlabel='t [%]', ylabel = 'Subjective Rating', title = rating)
 #plt.show()
 
 plt.savefig(save_plot+ file_name + '.png')
@@ -154,7 +182,6 @@ plt.savefig(save_plot+ file_name + '.png')
 #     print(label, grp)
 #     grp.plot(x = 't_', y = 'LZ', ax = axs[i], label = label)
 #     i = i+1
-
 
 
 
